930e051c4156aee429be7bde68655d93efe0c0b5
Andy Walter <musictopia2@hotmail.com>
2024-12-02 20:02:00 -0700

    Renamed variable.

--- Extensions/DotNetUpgradeExtensions.cs
+++ Extensions/DotNetUpgradeExtensions.cs
@@ -3,7 +3,7 @@ internal static class DotNetUpgradeExtensions
 {
     public static bool NeedsToUpdateVersion(this DotNetUpgradeConfigurationModel config)
     {
-        int possibleNewVersion = config.NetVersion + 1;
-        return DotNetVersionChecker.IsDotNetVersionInstalled(possibleNewVersion);
+        int targetVersion = config.NetVersion + 1;
+        return DotNetVersionChecker.IsDotNetVersionInstalled(targetVersion);
     }
 }
