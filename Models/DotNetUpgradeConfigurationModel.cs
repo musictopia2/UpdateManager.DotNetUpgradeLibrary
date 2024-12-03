c26c773d504c5b1c3311a2fd07c5a1563f0b20b4
Andy Walter <musictopia2@hotmail.com>
2024-12-02 19:59:26 -0700

    Code cleanup.

--- Models/DotNetUpgradeConfigurationModel.cs
+++ Models/DotNetUpgradeConfigurationModel.cs
@@ -5,5 +5,4 @@ public class DotNetUpgradeConfigurationModel
     public bool IsTestMode { get; set; }  // Determines if it's in test mode
     public string TestLocalFeedPath { get; set; } = "";  // Path to the test local feed
     public string TestPublicFeedPath { get; set; } = "";  // Path to the test public feed
-    //because i will have staging, no longer necessary to have the other temporary feed anymore
 }
