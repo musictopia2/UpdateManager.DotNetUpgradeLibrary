7e87ef2c4d5726c63a167dd3e8196f44e310e894
Andy Walter <musictopia2@hotmail.com>
2024-12-16 07:50:57 -0700

    Added ability to have interface that is like using when there are no pre processing.

---
+++ Interfaces/INoPreUpgradeProcessing.cs
@@ -0,0 +1,6 @@
+﻿namespace UpdateManager.DotNetUpgradeLibrary.Interfaces;
+public interface INoPreUpgradeProcessing : IUpgradePhaseHandler
+{
+    bool IUpgradePhaseHandler.Are1PreUpgradeProcessesNeeded() => false;
+    Task<bool> IUpgradePhaseHandler.Run1PreUpgradeProcessesAsync() => throw new CustomBasicException("No pre processes needed");
+}
