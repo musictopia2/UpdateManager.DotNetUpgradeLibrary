324a30f4bb46de1054cf91a88bab8b24ef183e27
Andy Walter <musictopia2@hotmail.com>
2024-12-16 13:11:10 -0700

    Added new upgrade status option.

--- Models/EnumUpgradeStatus.cs
+++ Models/EnumUpgradeStatus.cs
@@ -3,6 +3,7 @@ public enum EnumUpgradeStatus
 {
     None,              // No upgrade needed or no pre-upgrade process is applicable
     PreProcessed,      // Pre-upgrade processes are complete (e.g., .NET version and dependencies updated)
+    CheckedOut,        // Repository's default branch has been successfully checked out (or no repo)
     Built,             // Project successfully built (post-upgrade steps may still be pending)
     Completed          // Full upgrade (pre and post) process is completed
 }
